a4b08ea5b0ae9b68e2078fe9bbc5ec7358f0071b
parkjongsoo <newtein80@gmail.com>
2019-03-24 23:23:46 +0900

    Add design (important)

--- project-youtube-tutorial01/src/App.js
+++ project-youtube-tutorial01/src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 // import logo from "./logo.svg";
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import ProjectBoard from "./components/ProjectBoard";
+// import AddProjectTask from "./components/ProjectTask/AddProjectTask";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1 className="alert alert-warning">Welcome !! The changes are reflected immediately.!!</h1>
-      </div>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Route exact path="/" component={ProjectBoard} />
+
+          {/* <Route exact path="/addProjectTask" component={AddProjectTask} /> */}
+        </div>
+      </Router>
     );
   }
 }
